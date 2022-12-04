a82f0635c24deea9949491778ac1ba1465c2714d
Jonathon Leight <jonathon.leight@jleight.com>
2022-12-04 10:25:56 -0500

    Add solutions for day 4.

---
+++ csharp/src/AoC.Y2022/Solutions/D04P1.cs
@@ -0,0 +1,31 @@
+namespace AoC.Y2022.Solutions;
+
+[UsedImplicitly]
+public class D04P1 : ISolution
+{
+    public Task Run(SolutionContext context)
+    {
+        var result = context.InputLines
+            .Select(l => l.Split(","))
+            .Select(p => new
+            {
+                A = p[0].Split("-"),
+                B = p[1].Split("-")
+            })
+            .Select(p => new
+            {
+                A = new Range(int.Parse(p.A[0]), int.Parse(p.A[1])),
+                B = new Range(int.Parse(p.B[0]), int.Parse(p.B[1]))
+            })
+            .Count(p => p.A.Contains(p.B) || p.B.Contains(p.A));
+
+        Console.WriteLine(result);
+        return Task.CompletedTask;
+    }
+}
+
+file record Range(int Start, int End)
+{
+    public bool Contains(Range other)
+        => Start <= other.Start && End >= other.End;
+}

4a110a78d0f0c9c12e283652e7e9b1e3be12ccd3
Jonathon Leight <jonathon.leight@jleight.com>
2023-12-11 20:49:53 -0500

    Add solutions for day 9

---
+++ csharp/src/AoC.Y2023/Solutions/D09P1.cs
@@ -0,0 +1,44 @@
+namespace AoC.Y2023.Solutions;
+
+public class D09P1 : ISolution
+{
+    public Task Run(SolutionContext context)
+    {
+        var sum = 0L;
+
+        foreach (var line in context.InputLines)
+        {
+            var numbers = line
+                .Split(' ')
+                .Select(long.Parse)
+                .ToArray();
+
+            var derivatives = new List<long[]>
+            {
+                numbers
+            };
+
+            while (derivatives[^1].ToHashSet().Count > 1)
+                derivatives.Add(Derivative(derivatives[^1]));
+
+            var next = 0L;
+            for (var i = derivatives.Count - 1; i >= 0; i--)
+                next += derivatives[i][^1];
+
+            sum += next;
+        }
+
+        Console.WriteLine(sum);
+        return Task.CompletedTask;
+    }
+
+    private static long[] Derivative(long[] input)
+    {
+        var result = new long[input.Length - 1];
+
+        for (var i = 0; i < input.Length - 1; i++)
+            result[i] = input[i + 1] - input[i];
+
+        return result;
+    }
+}

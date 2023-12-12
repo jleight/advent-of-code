67ec17841192cdf0a299c21a5133f86ed66ab479
Jonathon Leight <jonathon.leight@jleight.com>
2023-12-11 19:04:21 -0500

    Add solutions for day 7

---
+++ csharp/src/AoC.Y2023/Solutions/D07P2.cs
@@ -0,0 +1,63 @@
+namespace AoC.Y2023.Solutions;
+
+public class D07P2 : ISolution
+{
+    public Task Run(SolutionContext context)
+    {
+        var sum = context
+            .InputLines
+            .Select(l => l.Split(' '))
+            .Select(l => new
+            {
+                Hand = l[0],
+                Bid = long.Parse(l[1]),
+                Strength = CalculateStrength(l[0])
+            })
+            .OrderBy(h => h.Strength)
+            .ThenBy(h => CalculateStrength(h.Hand[0]))
+            .ThenBy(h => CalculateStrength(h.Hand[1]))
+            .ThenBy(h => CalculateStrength(h.Hand[2]))
+            .ThenBy(h => CalculateStrength(h.Hand[3]))
+            .ThenBy(h => CalculateStrength(h.Hand[4]))
+            .Select((h, i) => h.Bid * (i + 1))
+            .Sum();
+
+        Console.WriteLine(sum);
+        return Task.CompletedTask;
+    }
+
+    private static int CalculateStrength(string hand)
+    {
+        var counts = hand
+            .GroupBy(c => c)
+            .Where(g => g.Key != 'J')
+            .Select(g => g.Count())
+            .OrderByDescending(c => c)
+            .ToArray();
+
+        return counts switch
+        {
+            [] => 7,
+            [_] => 7,
+            [_, 1] => 6,
+            [> 1, > 1] => 5,
+            [_, 1, 1] => 4,
+            [> 1, > 1, 1] => 3,
+            [_, _, _, _] => 2,
+            _ => 1
+        };
+    }
+
+    private static int CalculateStrength(char card)
+    {
+        return card switch
+        {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 1,
+            'T' => 10,
+            _ => card - '0'
+        };
+    }
+}

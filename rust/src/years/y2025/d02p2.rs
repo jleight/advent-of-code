54914dc155cd3abb4f4a8e013f580314c4df9794
Jonathon Leight <jonathon.leight@jleight.com>
2025-12-03 20:20:56 -0500

    Solve day 2 without a regex

--- rust/src/years/y2025/d02p2.rs
+++ rust/src/years/y2025/d02p2.rs
@@ -1,9 +1,6 @@
-use fancy_regex::Regex;
 use crate::utils::SolutionContext;
 
 pub fn solve(ctx: &SolutionContext) -> String {
-    let check = Regex::new(r"^(.+?)\1+$").expect("invalid regex");
-
     let mut sum = 0;
 
     for range in ctx.input.split(',') {
@@ -14,8 +11,27 @@ pub fn solve(ctx: &SolutionContext) -> String {
 
         for i in start..=end {
             let str = i.to_string();
+            let str_len = str.len();
 
-            if check.is_match(&str).expect("regex check failed") {
+            let half_len = str_len / 2;
+            
+            let mut success = false;
+
+            for j in 1..=half_len {
+                if str_len % j != 0 {
+                    continue;
+                }
+
+                let pattern = str[0..j].to_string();
+                let test = pattern.repeat(str_len / j);
+
+                if str == test {
+                    success = true;
+                    break;
+                }
+            }
+            
+            if success {
                 sum += i;
             }
         }
